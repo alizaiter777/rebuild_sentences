3a88d38331db17a2d8e19ccca090d5f86a64c152
alizaiter777 <alizaiter777@gmail.com>
2024-11-14 00:10:19 +0200

    Update exc.py

--- exc.py
+++ exc.py
@@ -1,9 +1,19 @@
+
+
 #define words list and lengths list 
 words=["the", "sky", "is", "blue"]
 lengths=[3, 2, 2, 1]
 #create a function that update char of word according to length
 def rebuild_sentence(words, lengths):
     new_sentence = []
+    for i in range(len(lengths)):
+        word = words[i]
+        length = lengths[i]
+        new_sentence.append(word[:length])
+
+    return ' '.join(new_sentence)
+
+print(rebuild_sentence(words, lengths))
     
 
 
