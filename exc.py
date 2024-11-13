64e13d2f68cf326e86b822225d3ee150aba749e0
alizaiter777 <alizaiter777@gmail.com>
2024-11-14 00:03:45 +0200

    create rebuild_sentence function

--- exc.py
+++ exc.py
@@ -1,3 +1,9 @@
 #define words list and lengths list 
 words=["the", "sky", "is", "blue"]
 lengths=[3, 2, 2, 1]
+#create a function that update char of word according to length
+def rebuild_sentence(words, lengths):
+    new_sentence = []
+    
+
+
